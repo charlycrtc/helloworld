b3521e0ac482a596e160e6d7f9cf26a6c29441f7
carloscrtc <crtc.carlos@gmail.com>
2013-08-08 16:34:46 -0500

    holaaa

---
+++ ImageTargets/obj/local/armeabi-v7a/objs-debug/ImageTargets/SampleUtils.o.d
@@ -0,0 +1,109 @@
+obj/local/armeabi-v7a/objs-debug/ImageTargets/SampleUtils.o: \
+ jni/SampleUtils.cpp jni/SampleUtils.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdio.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/log.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/math.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/limits.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/malloc.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/alloca.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/strings.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/memory.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h
+
+jni/SampleUtils.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/math.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/limits.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/strings.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/memory.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+C:/Users/carlos/Documents/adt-bundle-windows-x86_64-20130522/android-ndk-r9/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
